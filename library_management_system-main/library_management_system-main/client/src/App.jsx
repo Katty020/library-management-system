fdc62e9f9f4ce7aadf0be1552cbfcb2f70c6cbc8
Aryan Katiyar <131390724+Katty020@users.noreply.github.com>
2024-02-06 02:43:34 +0530

    Update App.jsx

--- library_management_system-main/library_management_system-main/client/src/App.jsx
+++ library_management_system-main/library_management_system-main/client/src/App.jsx
@@ -7,6 +7,71 @@ import Transactions from "./pages/Transactions/Transactions";
 import { loginAPI } from "./api";
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
+function App() {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const storedUserData = localStorage.getItem("user");
+  //   if (storedUserData) {
+  //     try {
+  //       const parsedUserData = JSON.parse(storedUserData);
+  //       setUser(parsedUserData);
+  //     } catch (error) {
+  //       console.error("Error parsing user data:", error);
+  //       localStorage.removeItem("user");
+  //     }
+  //   }
+  // }, []);
+
+  // const handleLogin = async (username, password) => {
+  //   try {
+  //     const response = await loginAPI(username, password);
+  //     localStorage.setItem("user", JSON.stringify(response.user));
+  //     setUser(response.user);
+  //     navigate('/'); // Redirect to the home page if successful login 
+      
+  //   } catch (error) {
+  //     toast.error("Invalid username or password!", {
+  //       position: "bottom-left",
+  //       autoClose: 5000,
+  //       hideProgressBar: false,
+  //       closeOnClick: true,
+  //       pauseOnHover: true,
+  //       draggable: true,
+  //       progress: undefined,
+  //       theme: "light",
+  //       });
+  //   }
+  // };
+
+  // const handleLogout = async () => {
+  //   try {
+  //     await axios.get("/logout", {
+  //       withCredentials: true, // Ensure credentials 
+  //     });
+  
+  //     // Clear the user data in localStorage or state
+  //     localStorage.removeItem('user');
+  //     setUser(null);
+  
+  //     // Clear cookies related to the session
+  //     document.cookie = "connect.sid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  
+  //     // Redirect or navigate the user to the login/sign-in page
+  //     navigate('/signin', { replace: true });
+  //   } catch (error) {
+  //     toast.error("Logout failed.", {
+  //       position: "bottom-left",
+  //       autoClose: 5000,
+  //       hideProgressBar: false,
+  //       closeOnClick: true,
+  //       pauseOnHover: true,
+  //       draggable: true,
+  //       progress: undefined,
+  //       theme: "light",
+  //       });    }
+  // };
 
 function App() {
   const [user, setUser] = useState(null);
